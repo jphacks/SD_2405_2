eb0434c4e532a9b1880cd169ae7050941dd32e45
mozuku55 <ryo.tako.05@gmail.com>
2024-10-27 02:07:13 +0900

    fix: set dev  host setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  ENV["HOSTS"]&.split(",")&.each do |host|
+    config.hosts << host
+  end
 end
